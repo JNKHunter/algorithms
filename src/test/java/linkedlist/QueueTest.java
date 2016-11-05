2098b50c5096bb08294d84d11cf50ea2277ec23b
John Hunter <john@pushagency.io>
2016-11-04 17:14:32 -0700

    added queuetest

---
+++ src/test/java/linkedlist/QueueTest.java
@@ -0,0 +1,16 @@
+package linkedlist;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by John on 11/4/16.
+ */
+public class QueueTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
