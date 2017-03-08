be9185fd7435c50b803b376e867c3b26465edeff
John Hunter <john@push-it-forward.com>
2017-03-07 18:57:57 -0800

    constructor

--- src/main/java/BruteCollinearPoints.java
+++ src/main/java/BruteCollinearPoints.java
@@ -6,6 +6,16 @@ public class BruteCollinearPoints {
     Point[] points;
 
     public BruteCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new NullPointerException("Point array cannot be null");
+        }
+
+        for(Point point : points) {
+            if (point == null ) {
+                throw new NullPointerException("Point cannot be null");
+            }
+        }
+
         this.points = points;
     }
 
