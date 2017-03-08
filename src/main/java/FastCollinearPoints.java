07006e98fcba81c0f4c1f0a072f9cdfcb8bb1f24
John Hunter <john@push-it-forward.com>
2017-03-08 13:43:11 -0800

    added fcp api

--- src/main/java/FastCollinearPoints.java
+++ src/main/java/FastCollinearPoints.java
@@ -2,4 +2,21 @@
  * Created by jhunter on 3/8/17.
  */
 public class FastCollinearPoints {
+    private Point[] points;
+
+    public FastCollinearPoints(Point[] points) {
+        this.points = points;
+    }
+
+    public int numberOfSegments(){
+
+        //TODO: Implement
+        return 0;
+    }
+
+    public LineSegment[] segments(){
+        //TODO: Implement
+        return null;
+    }
+
 }
