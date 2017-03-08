533121a645b9b1d2ae579dcf06b8f3a70b6cd4f6
John Hunter <john@push-it-forward.com>
2017-03-07 18:53:15 -0800

    added api

--- src/main/java/BruteCollinearPoints.java
+++ src/main/java/BruteCollinearPoints.java
@@ -2,4 +2,18 @@
  * Created by jhunter on 3/7/17.
  */
 public class BruteCollinearPoints {
+
+    Point[] points;
+
+    public BruteCollinearPoints(Point[] points) {
+        this.points = points;
+    }
+
+    public int numberOfSegments(){
+        return 0;
+    }
+
+    public LineSegment[] segments(){
+        return null;
+    }
 }
