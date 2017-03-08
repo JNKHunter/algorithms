28a532280dd049c26cb5de0532ab54c7d9025c22
John Hunter <john@push-it-forward.com>
2017-03-07 18:37:55 -0800

    added tests

--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -13,11 +13,17 @@ public class PointTest {
     private Point infiniteSlopePoint;
     private Point equalPoint;
     private Point originPoint;
+    private Point zeroSlopePointFurther;
+    private Point highSlopePoint;
+    private Point lowSlopePoint;
 
     @Before
     public void setUp(){
         point  = new Point(1,1);
+        highSlopePoint = new Point(2, 10);
+        lowSlopePoint = new Point(10, 2);
         zeroSlopePoint = new Point(5,1);
+        zeroSlopePointFurther = new Point(10,1);
         infiniteSlopePoint = new Point (1,5);
         equalPoint = new Point(1, 1);
         originPoint = new Point(0,0);
@@ -37,6 +43,13 @@ public class PointTest {
         assertEquals(0, point.compareTo(equalPoint));
         assertEquals(-1, point.compareTo(infiniteSlopePoint));
         assertEquals(1, point.compareTo(originPoint));
+    }
+
+    @Test
+    public void testComparator() throws Exception {
+        assertEquals(0, point.slopeOrder().compare(zeroSlopePoint, zeroSlopePointFurther));
+        assertEquals(1, point.slopeOrder().compare(highSlopePoint, lowSlopePoint));
+        assertEquals(-1, point.slopeOrder().compare(lowSlopePoint, highSlopePoint));
     }
 
 }
