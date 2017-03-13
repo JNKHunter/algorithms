0f7e1bf28e6e63f9f3b523c61d46a1e272d6f37e
John Hunter <john@push-it-forward.com>
2017-03-12 17:43:19 -0700

    updated

--- src/main/java/CollinearPointsClient.java
+++ src/main/java/CollinearPointsClient.java
@@ -29,7 +29,7 @@ public class CollinearPointsClient {
         StdDraw.show();
 
         // print and draw the line segments
-        FastCollinearPoints2 collinear = new FastCollinearPoints2(points);
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
 //        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
         for (LineSegment segment : collinear.segments()) {
             StdOut.println(segment);
