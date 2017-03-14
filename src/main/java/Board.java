39a1a035826bf2e785d5105e948d75d56596f907
John Hunter <john@push-it-forward.com>
2017-03-14 15:10:03 -0700

    added board classs api

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -2,4 +2,41 @@
  * Created by jhunter on 3/14/17.
  */
 public class Board {
+
+    public Board(int[][] blocks) {
+
+    }
+
+    public Board twin() {
+
+    }
+
+    public int dimension() {
+        return 0;
+    }
+
+    public int hamming() {
+        return 0;
+    }
+
+    public int manhattan() {
+        return 0;
+    }
+
+    public boolean isGoal() {
+        return false;
+    }
+
+    public boolean equals(Object y) {
+        return false;
+    }
+
+    /*public Iterable<Board> neighbors() {
+
+    }*/
+
+    public String toString() {
+        return this.toString();
+    }
+
 }
