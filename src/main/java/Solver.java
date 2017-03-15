6a95155405f03ea605d1086e68ba51515f5eb003
John Hunter <john@push-it-forward.com>
2017-03-15 10:25:06 -0700

    added Solver api

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -2,4 +2,20 @@
  * Created by jhunter on 3/15/17.
  */
 public class Solver {
+
+    public Solver(Board initial) {
+
+    }
+
+    public boolean isSolvabale() {
+        return false;
+    }
+
+    public int moves() {
+        return 0;
+    }
+
+    public Iterable<Board> solution() {
+        return null;
+    }
 }
