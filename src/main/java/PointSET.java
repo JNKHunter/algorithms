e82e3b07a6c62aab531ffedca097f6b5f25d4b04
John Hunter <john@push-it-forward.com>
2017-03-24 20:06:59 -0700

    added PointSet api

--- src/main/java/PointSET.java
+++ src/main/java/PointSET.java
@@ -1,5 +1,48 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
 /**
  * Created by jhunter on 3/21/17.
  */
 public class PointSET {
+
+    public PointSET() {
+
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void insert(Point2D p) {
+        if (p == null) {
+            throw new NullPointerException("Point Cannot Be Null");
+        }
+    }
+
+    public boolean contains(Point2D p) {
+        if (p == null) {
+            throw new NullPointerException("Point Cannot Be Null");
+        }
+        return false;
+    }
+
+    public void draw() {
+
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) {
+            throw new NullPointerException("Rect Cannot Be Null");
+        }
+        return null;
+    }
+
+    public Point2D nearest(Point2D p) {
+        return null;
+    }
 }
