5e6b0464784607520abdfc6096a61f1582792275
John Hunter <john@push-it-forward.com>
2017-04-11 19:18:29 -0700

    added arrayutil

---
+++ src/main/java/util/ArrayUtil.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.util.Random;
+
+/**
+ * Created by jhunter on 4/11/17.
+ */
+public class ArrayUtil {
+    public static int[] generateRandomArray(int arraySize, int minValue, int maxValue){
+        int[] theArray = new int[arraySize];
+
+        Random rand = new Random();
+
+        for(int i = 0; i < arraySize; i++){
+            theArray[i] = generateRandomInt(minValue, maxValue);
+        }
+
+        return theArray;
+    }
+
+    public static int generateRandomInt(int minValue, int maxValue){
+        Random rand = new Random();
+        return rand.nextInt((maxValue - minValue) + 1) + minValue;
+    }
+}
