1c117187fd5a66b23993a9753486fc1006602af4
John Hunter <john@push-it-forward.com>
2017-04-13 14:04:02 -0700

    completed find dupes

--- src/main/java/FindDuplicates.java
+++ src/main/java/FindDuplicates.java
@@ -1,5 +1,30 @@
+import util.ArrayUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by jhunter on 4/13/17.
  */
 public class FindDuplicates {
+
+
+    public static void main(String[] args) {
+        int[] values = ArrayUtil.generateRandomArray(10,1, 10);
+        Set<Integer> duplicatesSet = new HashSet<>();
+
+
+        for (int i = 0; i < values.length; i++) {
+
+            if (values[Math.abs(values[i]) - 1] > 0) {
+                values[Math.abs(values[i]) - 1] *= -1;
+            } else {
+                duplicatesSet.add(Math.abs(values[i]));
+            }
+        }
+
+        for (int duplicate: duplicatesSet) {
+            System.out.println(duplicate + "\n");
+        }
+    }
 }
